07cdf970f866d829961dbb5b73122f74332104f6
Mirza Chilman <mirzachilman@gmail.com>
2022-06-16 22:03:23 +0700

    adding react router for routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 
+import React from 'react'
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const ProductDetail = React.lazy(() => import('./routes/Product'))
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">
+          Home
+        </Link>
+        <Link to="/product/1">
+          Product
+        </Link>
+        <Routes>
+          <Route exact path="/product/:id" element={
+            <React.Suspense fallback="Loading. . .">
+              <ProductDetail />
+            </React.Suspense>
+          } />
+        </Routes>
       </header>
     </div>
   );
